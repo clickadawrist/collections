51894c60fd2d8ddbc1d1d73214697a4c0616e30f
yzhang64 <yzhang64@ncsu.edu>
2016-06-16 15:35:50 -0400

    ArrayList based lab

---
+++ Collections/src/edu/ncsu/csc216/collections/ArrayBasedList.java
@@ -0,0 +1,52 @@
+package edu.ncsu.csc216.collections;
+
+import java.util.AbstractList;
+
+public class ArrayBasedList extends AbstractList {
+
+	/* (non-Javadoc)
+	 * @see java.util.AbstractList#get(int)
+	 */
+	@Override
+	public Object get(int index) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	/* (non-Javadoc)
+	 * @see java.util.AbstractList#set(int, java.lang.Object)
+	 */
+	@Override
+	public Object set(int index, Object element) {
+		// TODO Auto-generated method stub
+		return super.set(index, element);
+	}
+
+	/* (non-Javadoc)
+	 * @see java.util.AbstractList#add(int, java.lang.Object)
+	 */
+	@Override
+	public void add(int index, Object element) {
+		// TODO Auto-generated method stub
+		super.add(index, element);
+	}
+
+	/* (non-Javadoc)
+	 * @see java.util.AbstractList#remove(int)
+	 */
+	@Override
+	public Object remove(int index) {
+		// TODO Auto-generated method stub
+		return super.remove(index);
+	}
+
+	/* (non-Javadoc)
+	 * @see java.util.AbstractCollection#size()
+	 */
+	@Override
+	public int size() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
