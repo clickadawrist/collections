bf61f40096b56a803b34fea49416848495fd9db6
magreen3 <magreen3@ncsu.edu>
2016-06-16 16:31:24 -0400

    Javadoc and create constructor, variables, and change "Object" to "E"

--- Collections/src/edu/ncsu/csc216/collections/ArrayBasedList.java
+++ Collections/src/edu/ncsu/csc216/collections/ArrayBasedList.java
@@ -2,51 +2,74 @@ package edu.ncsu.csc216.collections;
 
 import java.util.AbstractList;
 
-public class ArrayBasedList<E> extends AbstractList {
+/**
+ * Implementation of a generic array list extending a generic abstract list.
+ * @author Manaka Green and Jerry Zhang
+ * @param <E> Generic 
+ */
+public class ArrayBasedList<E> extends AbstractList<E> {
 
-	/* (non-Javadoc)
-	 * @see java.util.AbstractList#get(int)
+	/** Array of generic type elements. */
+	private E[] arrayBasedList;
+	/** Size of the collection. */
+	private int size;
+	
+	
+	/**
+	 * @param arrayBasedList
+	 * @param size
+	 * 
+	 */
+	@SuppressWarnings("unchecked")
+	public ArrayBasedList() {
+		//super();
+		arrayBasedList = (E[]) (new Object[10]);
+		this.size = 0;
+	}
+
+	/**
+	 * 
 	 */
 	@Override
-	public Object get(int index) {
-		// TODO Auto-generated method stub
+	public E get(int index) {
+		
 		return null;
 	}
 
-	/* (non-Javadoc)
-	 * @see java.util.AbstractList#set(int, java.lang.Object)
+	/**
+	 * 
 	 */
 	@Override
-	public Object set(int index, Object element) {
+	public E set(int index, E element) {
 		// TODO Auto-generated method stub
-		return super.set(index, element);
+		 return (E) super.set(index, element);
+		//return null;
 	}
 
-	/* (non-Javadoc)
-	 * @see java.util.AbstractList#add(int, java.lang.Object)
+	/**
+	 * 
 	 */
 	@Override
-	public void add(int index, Object element) {
+	public void add(int index, E element) {
 		// TODO Auto-generated method stub
 		super.add(index, element);
 	}
 
-	/* (non-Javadoc)
-	 * @see java.util.AbstractList#remove(int)
+	/**
+	 * 
 	 */
 	@Override
-	public Object remove(int index) {
+	public E remove(int index) {
 		// TODO Auto-generated method stub
-		return super.remove(index);
+		return (E) super.remove(index);
 	}
 
-	/* (non-Javadoc)
-	 * @see java.util.AbstractCollection#size()
+	/**
+	 *
 	 */
 	@Override
 	public int size() {
 		// TODO Auto-generated method stub
 		return 0;
 	}
-
 }
