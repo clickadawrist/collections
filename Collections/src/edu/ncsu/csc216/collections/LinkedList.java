3d348dd7219374fc466e0d172413251278950333
yzhang64 <yzhang64@ncsu.edu>
2016-06-29 15:56:18 -0400

    Create Node inner class.

--- Collections/src/edu/ncsu/csc216/collections/LinkedList.java
+++ Collections/src/edu/ncsu/csc216/collections/LinkedList.java
@@ -1,13 +1,28 @@
+/**
+ * 
+ */
 package edu.ncsu.csc216.collections;
 
 import java.util.AbstractList;
 
 /**
- * @author yijiezhang and Manaka Green
+ * @author yijiezhang
  *
  */
 public class LinkedList<E> extends AbstractList<E> {
+	private class Node<E> {
+		private E data;
+	    private Node<E> next;
 
+	    public Node(E d) {
+	        data = d;
+	        next = null;
+	    }
+	    public Node(E d, Node<E> n) {
+	        data = d;
+	        next = n;
+	    }
+	}
 	/* (non-Javadoc)
 	 * @see java.util.AbstractList#get(int)
 	 */
@@ -52,5 +67,4 @@ public class LinkedList<E> extends AbstractList<E> {
 		// TODO Auto-generated method stub
 		return 0;
 	}
-
 }
