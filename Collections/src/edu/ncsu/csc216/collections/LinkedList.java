5ad363de95d20d04960610cd0173766309469245
yzhang64 <yzhang64@ncsu.edu>
2016-06-29 15:45:08 -0400

    LinkedList lab.

---
+++ Collections/src/edu/ncsu/csc216/collections/LinkedList.java
@@ -0,0 +1,59 @@
+/**
+ * 
+ */
+package edu.ncsu.csc216.collections;
+
+import java.util.AbstractList;
+
+/**
+ * @author yijiezhang
+ *
+ */
+public class LinkedList<E> extends AbstractList<E> {
+
+	/* (non-Javadoc)
+	 * @see java.util.AbstractList#get(int)
+	 */
+	@Override
+	public E get(int index) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	/* (non-Javadoc)
+	 * @see java.util.AbstractList#set(int, java.lang.Object)
+	 */
+	@Override
+	public E set(int index, E element) {
+		// TODO Auto-generated method stub
+		return super.set(index, element);
+	}
+
+	/* (non-Javadoc)
+	 * @see java.util.AbstractList#add(int, java.lang.Object)
+	 */
+	@Override
+	public void add(int index, E element) {
+		// TODO Auto-generated method stub
+		super.add(index, element);
+	}
+
+	/* (non-Javadoc)
+	 * @see java.util.AbstractList#remove(int)
+	 */
+	@Override
+	public E remove(int index) {
+		// TODO Auto-generated method stub
+		return super.remove(index);
+	}
+
+	/* (non-Javadoc)
+	 * @see java.util.AbstractCollection#size()
+	 */
+	@Override
+	public int size() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
