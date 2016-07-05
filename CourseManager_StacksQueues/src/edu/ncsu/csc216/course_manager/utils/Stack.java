cb135289124f81469fd61985392aaee51c03890f
yzhang64 <yzhang64@ncsu.edu>
2016-07-05 15:54:52 -0400

    Create the Stack Interface.

---
+++ CourseManager_StacksQueues/src/edu/ncsu/csc216/course_manager/utils/Stack.java
@@ -0,0 +1,17 @@
+package edu.ncsu.csc216.course_manager.utils;
+
+/**
+ * Interface that specifies the stacks' behaviors.
+ * @author yijiezhang
+ * @param <E>
+ */
+public interface Stack<E> {
+	
+	public void push(E element);
+	
+	public E pop();
+	
+	public E peek();
+	
+	public boolean isEmplty();
+}
