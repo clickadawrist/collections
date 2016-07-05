a8b2d03be62aa84bc44eccf0a99c21cfc2e0d047
magreen3 <magreen3@ncsu.edu>
2016-07-05 16:08:45 -0400

    Create ArrayStackTest and LinkedStackTest.

---
+++ CourseManager_StacksQueues/test/edu/ncsu/csc216/course_manager/utils/LinkedStackTest.java
@@ -0,0 +1,26 @@
+package edu.ncsu.csc216.course_manager.utils;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * @author Manaka Green and Jerry Zhang
+ *
+ */
+public class LinkedStackTest {
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
