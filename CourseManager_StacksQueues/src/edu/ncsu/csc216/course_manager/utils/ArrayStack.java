369a70a953d18d91e6db178314ed0e6e9a1c4ed3
magreen3 <magreen3@ncsu.edu>
2016-07-05 15:59:26 -0400

    Make ArrayStack.

---
+++ CourseManager_StacksQueues/src/edu/ncsu/csc216/course_manager/utils/ArrayStack.java
@@ -0,0 +1,20 @@
+package edu.ncsu.csc216.course_manager.utils;
+
+import java.util.ArrayList;
+
+/**
+ * @author Manaka Green and Jerry Zhang
+ *
+ */
+public class ArrayStack<E> implements Stack {
+
+	private ArrayList<E> list = new ArrayList<E>();
+	
+	/**
+	 * 
+	 */
+	public ArrayStack() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
