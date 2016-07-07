a190f65b5027243afe9f9f4843838e7b97545289
yzhang64 <yzhang64@ncsu.edu>
2016-07-07 15:53:01 -0400

    Create the LinkedList<E> class.

---
+++ Collections/src/edu/ncsu/csc216/collections/LinkedList.java
@@ -0,0 +1,34 @@
+/**
+ * 
+ */
+package edu.ncsu.csc216.collections;
+
+import java.util.AbstractSequentialList;
+import java.util.ListIterator;
+
+/**
+ * 
+ * @author Yijie zhang and Manaka Green
+ */
+public class LinkedList<E> extends AbstractSequentialList<E> {
+	
+	/**
+	 * Returns a list iterator over the elements in this list (in proper sequence).
+	 * @param index index of first element to be returned 
+	 * from the list iterator (by a call to the next method)
+	 * @return a list iterator over the elements in this list (in proper sequence)
+	 * @throws IndexOutOfBoundsException if the index is out of range (index < 0 || index > size())
+	 */
+	@Override
+	public ListIterator<E> listIterator(int idx) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int size() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
