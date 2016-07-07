e1331645b558ea9ecb30b4da05f744cb1b88291a
magreen3 <magreen3@ncsu.edu>
2016-07-07 16:03:00 -0400

    Create LinkedListIterator inner class.

--- Collections/src/edu/ncsu/csc216/collections/LinkedList.java
+++ Collections/src/edu/ncsu/csc216/collections/LinkedList.java
@@ -50,4 +50,82 @@ public class LinkedList<E> extends AbstractSequentialList<E> {
 			this.next = next;
 		}
 	}
+	
+	/**
+	 * 
+	 * @author Manaka Green and Jerry Zhang
+	 *
+	 */
+	private class LinkedListIterator implements ListIterator<E> {
+
+		/**
+		 * 
+		 */
+		public LinkedListIterator() {
+			
+		}
+		
+		/**
+		 * 
+		 * @param index
+		 */
+		public LinkedListIterator(int index) {
+			
+		}
+		
+		@Override
+		public void add(E arg0) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public boolean hasNext() {
+			// TODO Auto-generated method stub
+			return false;
+		}
+
+		@Override
+		public boolean hasPrevious() {
+			// TODO Auto-generated method stub
+			return false;
+		}
+
+		@Override
+		public E next() {
+			// TODO Auto-generated method stub
+			return null;
+		}
+
+		@Override
+		public int nextIndex() {
+			// TODO Auto-generated method stub
+			return 0;
+		}
+
+		@Override
+		public E previous() {
+			// TODO Auto-generated method stub
+			return null;
+		}
+
+		@Override
+		public int previousIndex() {
+			// TODO Auto-generated method stub
+			return 0;
+		}
+
+		@Override
+		public void remove() {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void set(E arg0) {
+			// TODO Auto-generated method stub
+			
+		}
+		
+	}
 }
