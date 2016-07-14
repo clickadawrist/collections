a25a9e094d4b404e73757876b1c56f5a830a6257
yzhang64 <yzhang64@ncsu.edu>
2016-07-14 16:23:12 -0400

    Add javadoc for the methods.

--- Collections/src/edu/ncsu/csc216/collections/LinkedListRecursive.java
+++ Collections/src/edu/ncsu/csc216/collections/LinkedListRecursive.java
@@ -1,22 +1,22 @@
 package edu.ncsu.csc216.collections;
 
 /**
+ * A linkedlist behaves recursively.
  * @author Manaka Green and Jerry Zhang
- *
  */
 public class LinkedListRecursive<E> {
 
 	/**
-	 * 
+	 * Constructs a recursive linked list.
 	 */
 	public LinkedListRecursive() {
 		// TODO Auto-generated constructor stub
 	}
 	
 	/**
-	 * 
-	 * @param index
-	 * @return
+	 * Returns the element at the specified position in this list.
+	 * @param index index of the element
+	 * @return the element at the specified position in this list.
 	 */
 	public E get(int index) {
 		return null;
@@ -25,10 +25,10 @@ public class LinkedListRecursive<E> {
 	}
 	
 	/**
-	 * 
-	 * @param index
-	 * @param element
-	 * @return
+	 * Replaces the element at the specified position in this list with the specified element.
+	 * @param index index of the element
+	 * @param element the element will be replaced
+	 * @return the element at the specified position in this list with the specified element.
 	 */
 	public E set(int index, E element) {
 		return element;
@@ -37,9 +37,9 @@ public class LinkedListRecursive<E> {
 	}
 		
 	/**
-	 * 
-	 * @param index
-	 * @param element
+	 * Inserts the specified element at the specified position in this list.
+	 * @param index index of the element
+	 * @param element The element that will be inserted.
 	 */
 	public void add(int index, E element) {
 		// TODO Auto-generated method stub
@@ -47,9 +47,9 @@ public class LinkedListRecursive<E> {
 	}
 
 	/**
-	 * 
-	 * @param index
-	 * @return
+	 * Removes the element at the specified position in this list.
+	 * @param index index of the element
+	 * @return element that was removed
 	 */
 	public E remove(int index) {
 		return null;
@@ -58,8 +58,8 @@ public class LinkedListRecursive<E> {
 	}
 
 	/**
-	 * 
-	 * @return
+	 * Returns the number of elements in this list.
+	 * @return the number of elements in this list.
 	 */
 	public int size() {
 		// TODO Auto-generated method stub
