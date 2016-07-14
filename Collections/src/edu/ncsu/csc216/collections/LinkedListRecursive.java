ab807827b14762909333803614d88ddd4c738339
magreen3 <magreen3@ncsu.edu>
2016-07-14 15:51:28 -0400

    Initial creation of LinkedListRecursive.

---
+++ Collections/src/edu/ncsu/csc216/collections/LinkedListRecursive.java
@@ -0,0 +1,16 @@
+package edu.ncsu.csc216.collections;
+
+/**
+ * @author Manaka Green and Jerry Zhang
+ *
+ */
+public class LinkedListRecursive<E> {
+
+	/**
+	 * 
+	 */
+	public LinkedListRecursive() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
